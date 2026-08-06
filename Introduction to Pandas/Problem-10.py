79a87ed907cb4152f5ff831e59edaafabe23f46a
Ahmad Faraz <ahmadxfaraz@gmail.com>
2026-08-06 20:41:14 +0530

    Test after repository rename

--- Introduction to Pandas/Problem-10.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
-    students["grade"] = students["grade"].astype(int)
-    return students
-
-if __name__ == "__main__":
-    students = pd.DataFrame([[1, "John", 25, 85.5], [2, "Alice", 30, 90.0], [3, "Bob", 28, 78.0]], columns=["student_id", "name", "age", "grade"])
-    print(changeDatatype(students))
--- Introduction to Pandas/Problem-2.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def getDataframeSize(players: pd.DataFrame):
-    return list(players.shape)
-
-if __name__ == "__main__":
-    players = pd.DataFrame([[1, 20], [2, 21], [3, 22]], columns=["player_id", "age"])
-    print(getDataframeSize(players))
